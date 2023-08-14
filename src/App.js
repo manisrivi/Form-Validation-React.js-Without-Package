5a4e7cbd1487a421d7f38fc82ea21d28ba81e7dc
MANIKANDAN S <95971849+manisrivi@users.noreply.github.com>
2023-08-14 11:48:11 +0000

    Final

--- src/App.js
+++ src/App.js
@@ -1,124 +1,131 @@
-import "./styles.css";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 export default function App() {
-  // Initial form data
-  const [formData, setFormData] = useState({
-    name: "",
+  // Initial values
+  const initialValues = {
+    username: "",
+    phonenumber: "",
     email: "",
     password: ""
-  });
-  // Error form data
-  const [errors, setErrors] = useState({
-    name: "",
-    email: "",
-    password: ""
-  });
-  // Touched and Untouched form data
-  const [touched, setTouched] = useState({
-    name: false,
-    email: false,
-    password: false
-  });
-  // handleChange for Input data
+  };
+  // form values set an initial state
+  const [formValues, setFormValues] = useState(initialValues);
+  // form error handling
+  const [errorFormValues, setErrorFormValues] = useState({});
+  // form submit or not submit
+  const [isSubmit, setIsSubmit] = useState(false);
+  // form handle change
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-    setErrors({
-      ...errors,
-      [e.target.name]: ""
-    });
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value
+    }));
+    setIsSubmit(true);
   };
-  // handle blur
-  const handleBlur = (e) => {
-    setTouched({
-      ...touched,
-      [e.target.value]: true
-    });
-  };
-  // Validation
-  const validation = () => {
-    // Error Initialized
-    let tempErrors = {};
-    let formIsValid = true;
-    // name Validate
-    if (!formData.name) {
-      formIsValid = false;
-      tempErrors.name = "Name is Empty";
-    }
-    // email Validate
-    if (!formData.email) {
-      formIsValid = false;
-      tempErrors.email = "Email is Empty";
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      formIsValid = false;
-      tempErrors.email = "Email is Invalid";
-    }
-    // password Validate
-    if (!formData.password) {
-      formIsValid = false;
-      tempErrors.password = "Password is Empty";
-    }
-    setErrors(tempErrors);
-    return formIsValid;
-  };
-  // handleSubmit
+  // form submit
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (validation()) {
-      console.log(formData);
-    }
+    setErrorFormValues(validate(formValues));
   };
-
+  const handleBlur = (e) => {
+    const { name } = e.target;
+    setErrorFormValues((prevErrors) => ({
+      ...prevErrors,
+      [name]: validateField(name, formValues[name])
+    }));
+  };
+  // checkn the length
+  useEffect(() => {
+    if (Object.keys(formValues).length === 0 && isSubmit) {
+    }
+  }, []);
+  // validation
+  const validate = (values) => {
+    const errors = {};
+    const Emailregex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const Numberregex = /^(0|[1-9][0-9]*)$/;
+    if (!values.username) {
+      errors.username = "User name is Required!";
+    }
+    if (!values.phonenumber) {
+      errors.phonenumber = "Phone number is Required!";
+    } else if (!Numberregex.test(values.phonenumber)) {
+      errors.phonenumber = "Not a Number!";
+    }
+    if (!values.email) {
+      errors.email = "Email is Required!";
+    } else if (!Emailregex.test(values.email)) {
+      errors.email = "This is not valid email!";
+    }
+    if (!values.password) {
+      errors.password = "Password is Required!";
+    }
+    return errors;
+  };
+  const validateField = (fieldName, value) => {
+    if (!value) {
+      return `${fieldName} is Required!`;
+    }
+    return "";
+  };
   return (
     <div className="container">
-      <form className="d-flex flex-column gap-3 p-3" onSubmit={handleSubmit}>
-        {/* Name */}
-        <div>
-          <span>Name</span>
+      <h1 className="text-center">Login Form</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="d-flex flex-column gap-2 mt-5 p-4 border rounded shadow-lg"
+      >
+        {/* Username */}
+        <div className="text-center d-flex flex-column">
+          <label className="text-start">User name</label>
           <input
-            type="text"
             className="form-control"
-            name="name"
-            value={formData.name}
+            name="username"
+            value={formValues.username}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {<span className="text-danger">{errors.name}</span>}
+          <span className="text-danger">{errorFormValues.username}</span>
+        </div>
+        {/* Username */}
+        <div className="text-center d-flex flex-column">
+          <label className="text-start">Phone number</label>
+          <input
+            className="form-control"
+            name="phonenumber"
+            value={formValues.phonenumber}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <span className="text-danger">{errorFormValues.phonenumber}</span>
         </div>
         {/* Email */}
-        <div>
-          <span>Email</span>
+        <div className="text-center d-flex flex-column">
+          <label className="text-start">Email</label>
           <input
-            type="email"
             className="form-control"
             name="email"
-            value={formData.email}
+            value={formValues.email}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {<span className="text-danger">{errors.email}</span>}
+          <span className="text-danger">{errorFormValues.email}</span>
         </div>
         {/* Password */}
-        <div>
-          <span>Password</span>
+        <div className="text-center d-flex flex-column">
+          <label className="text-start">Password</label>
           <input
-            type="password"
             className="form-control"
             name="password"
-            value={formData.password}
+            value={formValues.password}
             onChange={handleChange}
             onBlur={handleBlur}
           />
-          {<span className="text-danger">{errors.password}</span>}
+          <span className="text-danger">{errorFormValues.password}</span>
         </div>
         {/* Submit */}
-        <div>
-          <button type="submit" className="btn btn-info form-control">
-            Submit
-          </button>
+        <div className="text-center d-flex flex-column">
+          <button className="form-control btn btn-info">Submit</button>
         </div>
       </form>
     </div>
